955f4f17b968afdead9d959505646ca09261cd01
Kirill <kirill.herz@gmail.com>
2018-10-25 00:35:29 +0300

    Функция avr переписана
    Функция avr переписана с использованием функции reduce для поиска среднего арифметического.

--- arr_algs.py
+++ arr_algs.py
@@ -1,19 +1,12 @@
 from functools import reduce
 
 
-def avr(arr):
-    if arr:
-        a = 0
-        for el in arr:
-            a = a + el
-        n = len(arr)
-        return a / n
-    else:
-        print('Массив пуст!')
 def find_min(array):
     return reduce(lambda accumulator, item: item if item < accumulator else accumulator, array)
 
 
+def avr(array):
+    return reduce(lambda accumulator, item: accumulator + item, array) / (float)(len(array))
 
 
 a = [2, 8, 9, 11, 78, 1]
