3aa161a7c561f4f1e858b32664e4687dca636618
Kirill <kirill.herz@gmail.com>
2018-10-25 01:12:15 +0300

    Добавлено возбуждение исключений
    Добавлено возбуждение исключений в случае если аргумент не list либо list пустой

--- arr_algs.py
+++ arr_algs.py
@@ -1,11 +1,20 @@
 from functools import reduce
 
 
+def raise_exception_for_incorrect_list(array):
+    if not type(array) is list:
+        raise TypeError("argument have not type list")
+    elif not array:
+        raise ValueError("list is empty")
+
+
 def find_min(array):
+    raise_exception_for_incorrect_list(array)
     return reduce(lambda accumulator, item: item if item < accumulator else accumulator, array)
 
 
 def avr(array):
+    raise_exception_for_incorrect_list(array)
     return reduce(lambda accumulator, item: accumulator + item, array) / (float)(len(array))
 
 
